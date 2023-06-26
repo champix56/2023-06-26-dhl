a7a0f7fcf891be8385e7d4815421ee0852fba60e
champix56 <desorbaix@free.fr>
2023-06-26 16:29:59 +0200

    fonction avec jsdoc

--- js/index.js
+++ js/index.js
@@ -1,24 +1,8 @@
-var uneVariableLambda = 23;
-var UNE_VALEUR_CONST = Object.freeze({});
-function onbuttonclick(evt) {
-  //corps de la fonction
-  console.log(evt);
+/**
+ * fonction d'initialisation
+ * @returns {undefined} aucun retour
+ */
+function init() {
+    console.log('hello');
 }
-onbuttonclick("coucou");
-var obj = {
-  a: 123,
-  b: function () {
-    console.log("hello");
-  },
-};
-obj.a = 254;
-obj.b();
-var str = '<a href="bdfb">le liens d\'alex</a>';
-var nb = 125;
-var arr = [123, 254, {}, ""];
-console.log(arr[0]);
-var a = true;
-if (a) {
-  console.log("a est vrai");
-}
-
+init()
