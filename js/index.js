9d49ff0c9c8698356f98d0d1a5d2779e6f8b4bf2
champix56 <desorbaix@free.fr>
2023-06-26 16:23:17 +0200

    convention

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-var uneVariableLambda=23;
-var UNE_VALEUR_CONST = Object.freeze({})
-function onbuttonclick(evt){
+var uneVariableLambda = 23;
+var UNE_VALEUR_CONST = Object.freeze({});
+function onbuttonclick(evt) {
+  //corps de la fonction
+  console.log(evt);
 }
+onbuttonclick("coucou");
+var obj = {
+  a: 123,
+  b: function () {
+    console.log("hello");
+  },
+};
+obj.a = 254;
+obj.b();
+var str = '<a href="bdfb">le liens d\'alex</a>';
+var nb = 125;
+var arr = [123, 254, {}, ""];
+console.log(arr[0]);
+var a = true;
+if (a) {
+  console.log("a est vrai");
+}
+
