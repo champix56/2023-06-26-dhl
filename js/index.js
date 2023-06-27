5327754ca65dfd2b78f991b0a7be1e511fc9c095
champix56 <desorbaix@free.fr>
2023-06-27 09:16:28 +0200

    moddif de content de balise

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@
  * @returns {undefined} aucun retour
  */
 function init() {
-    console.log('hello');
+    var currentDate=new Date();
+    console.log(currentDate.toISOString());
+    var footer=document.getElementsByTagName('footer')[0];
+    footer.innerHTML=currentDate.toISOString();
 }
-init()
+
+init();
