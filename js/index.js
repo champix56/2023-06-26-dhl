cb943040743519ae4eb8d706c2121fd0a7129530
champix56 <desorbaix@free.fr>
2023-06-27 10:37:54 +0200

    reformat

--- js/index.js
+++ js/index.js
@@ -3,17 +3,18 @@
  * @returns {undefined} aucun retour
  */
 function init() {
-    var currentDate=new Date();
-    console.log(currentDate.toISOString());
-    var footer=document.getElementsByTagName('footer')[0];
-    footer.innerHTML=currentDate.toISOString();
-    footer.style.backgroundColor='rgba(128,128,100,0.3)';
-    footer.style.color='white';
-    footer.style.fontStyle='italic';
-    footer.style.fontWeight='900';
-    footer.style.textDecoration='underline';
+  var currentDate = new Date();
+  console.log(currentDate.toISOString());
+  var footer = document.getElementsByTagName("footer")[0];
+  footer.innerHTML = currentDate.toISOString();
+  footer.style.backgroundColor = "rgba(128,128,100,0.3)";
+  footer.style.color = "white";
+  footer.style.fontStyle = "italic";
+  footer.style.fontWeight = "900";
+  footer.style.textDecoration = "underline";
 }
-document.addEventListener('DOMContentLoaded',function(evt){
-    console.log(evt);
-    init();
-})
+
+document.addEventListener("DOMContentLoaded", function (evt) {
+  console.log(evt);
+  init();
+});
