42bad6430140fbefa769b97e8331fe4a30b8fd2b
champix56 <desorbaix@free.fr>
2023-06-27 09:47:02 +0200

    manip du style css de balise par js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ function init() {
     console.log(currentDate.toISOString());
     var footer=document.getElementsByTagName('footer')[0];
     footer.innerHTML=currentDate.toISOString();
+    footer.style.backgroundColor='rgba(128,128,100,0.3)';
+    footer.style.color='white';
+    footer.style.fontStyle='italic';
+    footer.style.fontWeight='900';
+    footer.style.textDecoration='underline';
 }
 document.addEventListener('DOMContentLoaded',function(evt){
     console.log(evt);
