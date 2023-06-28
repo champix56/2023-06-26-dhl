52042617059ea8a344ceb623f6b2ab1b07bf3557
champix56 <desorbaix@free.fr>
2023-06-28 19:40:27 +0200

    init complet de l'editor

--- js/js-views/editor.js
+++ js/js-views/editor.js
@@ -19,46 +19,46 @@ const initFormEvent = () => {
   var form = document.forms["meme-form"];
   form["titre"].addEventListener("input", function (evt) {
     currentMeme.titre = evt.target.value;
-    // Meme.renderMeme(currentMeme,svgViewerSelector,currentImage);
+    // Meme.render(currentMeme,svgViewerSelector,currentImage);
   });
   form["imageId"].addEventListener("change", function (evt) {
     currentMeme.imageId = Number(evt.target.value);
     currentImage = ressources.images.find(
       (img) => img.id === currentMeme.imageId
     );
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["text"].addEventListener("input", function (evt) {
     currentMeme.text = evt.target.value;
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["x"].addEventListener("input", function (evt) {
     currentMeme.x = Number(evt.target.value);
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["y"].addEventListener("input", function (evt) {
     currentMeme.y = Number(evt.target.value);
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["color"].addEventListener("input", function (evt) {
     currentMeme.color = evt.target.value;
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["fontSize"].addEventListener("input", function (evt) {
     currentMeme.fontSize = Number(evt.target.value);
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["fontWeight"].addEventListener("input", function (evt) {
     currentMeme.fontWeight = evt.target.value;
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["underline"].addEventListener("input", function (evt) {
     currentMeme.underline = evt.target.checked;
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
   form["italic"].addEventListener("input", function (evt) {
     currentMeme.italic = evt.target.checked;
-    Meme.renderMeme(currentMeme, svgViewerSelector, currentImage);
+    Meme.render(currentMeme, svgViewerSelector, currentImage);
   });
 };
 const initFormValues = () => {
@@ -83,15 +83,15 @@ const reloadImageSelect = () => {
     select.appendChild(opt);
     ressources.images.forEach(img=>{
         const optCloned=opt.cloneNode(true);
-        optCloned=opt.value=img.id;
-        optCloned=opt.innerHTML=img.titre;
+        optCloned.value=img.id;
+        optCloned.innerHTML=img.titre;
         select.appendChild(optCloned)
     })
 };
 export const setCurrentMeme = (meme) => {
   currentMeme = meme;
   currentImage = ressources.images.find(
-    (img) => img.id === currentImage.imageId
+    (img) => img.id === currentMeme.imageId
   );
   initFormValues();
   Meme.render(currentMeme,svgViewerSelector,currentImage);
