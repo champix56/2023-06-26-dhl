6747100f5a89d9b8f429492262ff1ee0cd4ba57e
champix56 <desorbaix@free.fr>
2023-06-28 18:55:11 +0200

    loading avec callback des ressources

--- js/metier/Ressources.js
+++ js/metier/Ressources.js
@@ -1,9 +1,19 @@
+import {REST_ADR, ressourcesPaths} from '../constantes.js'
 export class Ressources{
     #images=[]
     #memes=[]
     get memes(){return this.#memes}
     get images(){return this.#images}
-    loadRessources(){
-
+    loadRessources(callback){
+        const pri=fetch(`${REST_ADR}${ressourcesPaths.images}`).then(rsp=>Response.json())
+        const prm=fetch(`${REST_ADR}${ressourcesPaths.memes}`).then(rsp=>Response.json())
+        Promise.all([pri,prm]).then(arr=>{
+            this.#images.splice(0);
+            this.#images.push(...arr[0]);
+            this.#memes.splice(0);
+            this.#memes.push(...arr[1])
+            callback(this);
+        })
     }
 }
+export const ressources=new Ressources();
