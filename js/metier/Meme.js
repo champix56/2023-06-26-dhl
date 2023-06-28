80304670e5d4f87b0cf7d83e29ecee08260ad857
champix56 <desorbaix@free.fr>
2023-06-28 15:36:24 +0200

    Meme.js

---
+++ js/metier/Meme.js
@@ -0,0 +1,12 @@
+export class Meme{
+    titre='';
+    text='';
+    x=0;
+    y=20;
+    imageId=-1;
+    fontSize=20;
+    fontWeight='500';
+    underline=false;
+    italic=false;
+    color='#000000'
+}
