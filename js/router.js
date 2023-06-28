b24881a7c89cfef8e31807cd4558830f0c27cd71
champix56 <desorbaix@free.fr>
2023-06-28 10:58:50 +0200

    recup du chemin en cours location

---
+++ js/router.js
@@ -0,0 +1,15 @@
+/**
+ * variable de config des routes
+ */
+const routeConfig = {
+  routes: [],
+};
+/**
+ * fonction de maintient de la route
+ */
+export const handleRoute = () => {
+  const pathName = location.pathname;
+  console.log(pathName);
+};
+
+
