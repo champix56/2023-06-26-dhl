dc70c19377dc8a9b2307bfa64e823342196b6306
champix56 <desorbaix@free.fr>
2023-06-28 12:51:16 +0200

    insertion dans le DOM de contenu Text deja présent

--- js/router.js
+++ js/router.js
@@ -11,6 +11,11 @@ const routeConfig = {
     {
       path: "/",
       templateUrl: "/view/home.html",
+      templateText:
+        '\
+      <div id="home">\
+        <button>benjamin</button>\
+      </div>',
     },
     {
       path: "/break",
@@ -35,11 +40,20 @@ class Router {
     this.#curentRoute = routeConfig.routes.find(
       (route) => route.path === pathName
     );
+    this.#loadCurrentDOMContent();
   }
   /**
    * navigate to
    * @param {string} pathName chemin commencant par /
    */
   changeRoute(pathName) {}
+  /**
+   * chargement du contenu text/html de la vue dans le noeuds du selecteur en parametre
+   * @param {string} domContainerSelector css selecteur du noeud a charger pour la vue
+   */
+  #loadCurrentDOMContent(domContainerSelector = "article") {
+    document.querySelector(domContainerSelector).innerHTML =
+      this.#curentRoute.templateText;
+  }
 }
 export const router = new Router();
