077a3d3eef96e352506c11aa955de2ccf65b6ad8
champix56 <desorbaix@free.fr>
2023-06-28 19:02:59 +0200

    fonction static de class

--- js/metier/Meme.js
+++ js/metier/Meme.js
@@ -1,12 +1,31 @@
-export class Meme{
-    titre='';
-    text='';
-    x=0;
-    y=20;
-    imageId=-1;
-    fontSize=20;
-    fontWeight='500';
-    underline=false;
-    italic=false;
-    color='#000000'
+export class Meme {
+  titre = "";
+  text = "";
+  x = 0;
+  y = 20;
+  imageId = -1;
+  fontSize = 20;
+  fontWeight = "500";
+  underline = false;
+  italic = false;
+  color = "#000000";
+  static render(meme, svgSelector, image) {
+    const svg = document.querySelector(svgSelector);
+    svg.setAttribute(
+      "viewBox",
+      `0 0 ${undefined !== image ? image.w : 500} ${
+        undefined !== image ? image.h : 500
+      }`
+    );
+    const svgimg=svg.querySelector('image');
+    svgimg.setAttribute('xlink:href',undefined!==image?image.url:'');
+    const text=svg.querySelector('text');
+    text.style.x=meme.x;
+    text.style.y=meme.y;
+    text.style.fill=meme.color;
+    text.style.textDecoration=meme.underline?'underline':'none';
+    text.style.fontStyle=meme.italic?'italic':'normal';
+    text.style.fontSize=meme.fontSize;
+    text.style.fontWeight=meme.fontWeight;
+  }
 }
