b77932b84d670f77221facd5bb694accb82afa48
champix56 <desorbaix@free.fr>
2023-06-28 16:30:44 +0200

    install json-server

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ document.addEventListener('DOMContentLoaded',(evt)=>{
     router.handleRoute()
     console.log(router.currentRoute)
 })
+
+// const f=async()=>{
+//     const pr=await fetch('http://localhost:5679/memes')
+//     const r=await pr.json();
+//     console.log(r)
+// }
