b38bfb12135336dd5e244670ccc2c37635e49d95
champix56 <desorbaix@free.fr>
2023-06-29 09:51:42 +0200

    load Select images

--- js/js-views/editor.js
+++ js/js-views/editor.js
@@ -14,5 +14,18 @@ const setCurrentMeme=()=>{
 
 }
 const initSelectImages=()=>{
-   
+    var select = document.forms["meme-form"]["imageId"];
+     select.innerHTML = "";
+  
+    var optBase = document.createElement("option");
+    optBase.value = "-1";
+    optBase.innerHTML = "only text";
+    select.appendChild(optBase);
+
+    ressources.images.forEach(function (img) {
+      var opt = optBase.cloneNode(true);
+      opt.value = img.id;
+      opt.innerHTML = img.titre;
+      select.appendChild(opt);
+    });
 }
