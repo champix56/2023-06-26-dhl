15f78c106c1c78ec5d74e7c2f21e8f9dff73d39f
champix56 <desorbaix@free.fr>
2023-06-29 09:50:58 +0200

    ajout du loading par fetch et sync

--- js/metier/Ressources.js
+++ js/metier/Ressources.js
@@ -1,9 +1,33 @@
-export class Ressources{
-    #images=[]
-    #memes=[]
-    get memes(){return this.#memes}
-    get images(){return this.#images}
-    loadRessources(){
+import REST_ADR, { RESSOUORCE_PATH } from "../constantes.js";
 
-    }
+export class Ressources {
+  #isLoaded = false;
+  #images = [];
+  #memes = [];
+  get memes() {
+    return this.#memes;
+  }
+  get images() {
+    return this.#images;
+  }
+  get isLoaded() {
+    return this.#isLoaded;
+  }
+  loadRessources() {
+    const promiseImages = fetch(REST_ADR + RESSOUORCE_PATH.images).then(
+      (resp) => resp.json()
+    );
+    const promiseMemes = fetch(REST_ADR + RESSOUORCE_PATH.memes).then((resp) =>
+      resp.json()
+    );
+    Promise.all([promiseImages, promiseMemes]).then((array) => {
+      console.log(array);
+      this.#images.splice(0);
+      this.#images.push(...array[0]);
+      this.#memes.splice(0);
+      this.#memes.push(...array[1]);
+      this.#isLoaded=true;
+    });
+  }
 }
+export const ressource = new Ressources();
