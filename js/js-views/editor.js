872d9fcfa50ef15f0c07940540d868856da91c97
champix56 <desorbaix@free.fr>
2023-06-29 09:51:25 +0200

    def des besoins de vue formulaire

---
+++ js/js-views/editor.js
@@ -0,0 +1,18 @@
+import { ressource } from "../metier/Ressources.js";
+let currentMeme;
+const VIEW_EDITOR_CSS_SELECTOR="#editor"
+export const initEditor=()=>{
+
+}
+const initFormEvent=()=>{
+
+}
+const initFormValues=()=>{
+
+}
+const setCurrentMeme=()=>{
+
+}
+const initSelectImages=()=>{
+   
+}
