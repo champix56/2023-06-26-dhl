7cb713d198e661cedad29ae3e169aef5fbc77d30
champix56 <desorbaix@free.fr>
2023-06-29 10:55:46 +0200

    initEditor

--- js/js-views/editor.js
+++ js/js-views/editor.js
@@ -1,31 +1,32 @@
-import { ressource } from "../metier/Ressources.js";
+import { ressources } from "../metier/Ressources.js";
 let currentMeme;
-const VIEW_EDITOR_CSS_SELECTOR="#editor"
-export const initEditor=()=>{
-
-}
-const initFormEvent=()=>{
-
-}
-const initFormValues=()=>{
-
-}
-const setCurrentMeme=()=>{
-
-}
-const initSelectImages=()=>{
-    var select = document.forms["meme-form"]["imageId"];
-     select.innerHTML = "";
-  
-    var optBase = document.createElement("option");
-    optBase.value = "-1";
-    optBase.innerHTML = "only text";
-    select.appendChild(optBase);
-
-    ressources.images.forEach(function (img) {
-      var opt = optBase.cloneNode(true);
-      opt.value = img.id;
-      opt.innerHTML = img.titre;
-      select.appendChild(opt);
+const VIEW_EDITOR_CSS_SELECTOR = "#editor";
+export const initEditor = () => {
+  if (ressources.isLoaded) {
+    initSelectImages();
+  }
+  else{
+    ressources.loadRessources((res)=>{
+        initSelectImages()
     });
-}
+  }
+};
+const initFormEvent = () => {};
+const initFormValues = () => {};
+const setCurrentMeme = () => {};
+const initSelectImages = () => {
+  var select = document.forms["meme-form"]["imageId"];
+  select.innerHTML = "";
+
+  var optBase = document.createElement("option");
+  optBase.value = "-1";
+  optBase.innerHTML = "only text";
+  select.appendChild(optBase);
+
+  ressources.images.forEach(function (img) {
+    var opt = optBase.cloneNode(true);
+    opt.value = img.id;
+    opt.innerHTML = img.titre;
+    select.appendChild(opt);
+  });
+};
